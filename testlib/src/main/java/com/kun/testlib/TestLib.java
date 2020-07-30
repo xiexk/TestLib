24e3424eab683794b7b4e91ea8a87e89e56a2067
DESKTOP-DHTJT6U\kun <ws1349010832@gmail.com>
2020-07-30 09:52:38 +0800

    测试公告仓库添加共有依赖包

---
+++ testlib/src/main/java/com/kun/testlib/TestLib.java
@@ -0,0 +1,13 @@
+package com.kun.testlib;
+
+/**
+ * @author kun
+ * Date: 2020/7/30
+ * Time: 9:43
+ */
+public class TestLib {
+
+    public static void pri(String str){
+        System.out.println(str);
+    }
+}
